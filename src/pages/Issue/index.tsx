aa37c88701a24f372d9bba153143fa3dc82541bd
Caio Vinícius <caio1525pereira@gmail.com>
2022-08-25 18:59:12 -0300

    feat: :construction: Conteúdo da issue

--- src/pages/Issue/index.tsx
+++ src/pages/Issue/index.tsx
@@ -1,7 +1,22 @@
 import { IssueCard } from "./components/IssueCard";
 
+import { IssueContentContainer } from "./styles";
+
 function Issue() {
-  return <IssueCard />;
+  return (
+    <>
+      <IssueCard />
+
+      <IssueContentContainer>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime ipsa
+          eaque error nisi, reprehenderit repudiandae qui porro aliquid nemo,
+          officiis dolorem. Officia sit deserunt consequuntur eveniet, doloribus
+          odio voluptatum aspernatur.
+        </p>
+      </IssueContentContainer>
+    </>
+  );
 }
 
 export { Issue };
---
+++ src/pages/Issue/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const IssueContentContainer = styled.section`
+  padding: 2.5rem 2rem;
+  color: ${(props) => props.theme["base-text"]};
+  word-break: break-all;
+`;
