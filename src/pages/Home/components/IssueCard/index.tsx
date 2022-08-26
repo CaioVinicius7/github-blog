64ea1cbc65c7b1987a36edac433b8cbc8b86eba1
Caio Vinícius <caio1525pereira@gmail.com>
2022-08-26 14:56:11 -0300

    refactor: :hammer: Formatação de texto do IssueCard usando regex

--- src/pages/Home/components/IssueCard/index.tsx
+++ src/pages/Home/components/IssueCard/index.tsx
@@ -4,6 +4,8 @@ import { IssueCardContainer } from "./styles";
 
 import { distanceToNow } from "../../../../lib/dateFns";
 
+import { removeSpecialCharacters } from "../../../../utils/regex";
+
 interface IssueCardProps extends HTMLAttributes<HTMLDivElement> {
   title: string;
   content: string;
@@ -16,7 +18,9 @@ function IssueCard({ title, content, createdAt, ...props }: IssueCardProps) {
   const shortenedTitle =
     title.length > 38 ? title.substring(0, 38).concat("...") : title;
 
-  const shortenedContent = content.substring(0, 181).concat("...");
+  const markdownToText = content.replace(removeSpecialCharacters, " ");
+
+  const shortenedContent = markdownToText.substring(0, 181).concat("...");
 
   return (
     <IssueCardContainer {...props}>
---
+++ src/utils/regex.ts
@@ -0,0 +1,2 @@
+export const removeSpecialCharacters =
+  /\((.*?)\)|([\u0300-\u036f]|[^0-9a-zA-Z])/g;
